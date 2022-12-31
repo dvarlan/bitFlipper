b9c00886ea6b8aa28960b6b8c7757c2a5c55b169
Dani <daniel.varlan20@gmail.com>
2022-12-31 15:49:34 +0100

    Added new Upgrade
    + Minor css changes

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const bitBtn = document.getElementById("bitbtn")
 const bitText = document.getElementById("bits")
 const cpuBtn = document.getElementById("cpubtn")
 const cpuStats = document.getElementById("cpustats")
+const gpuBtn = document.getElementById("gpubtn")
+const gpuStats = document.getElementById("gpustats")
 
 let currentClickAmount = 1
 let bits = 0
+let timer = -1337
 
 let cpuCost = 50
 let cpuPower = 1
 let cpuLvl = 0
+
+let gpuCost = 100
+let gpuPower = 0
+let gpuLvl = 0
 
 function render() {
     bitText.innerText = "Bits: " + bits
@@ -17,6 +24,12 @@ function render() {
 
 function renderStats() {
     cpuStats.innerText = "CPU Level: " + cpuLvl
+    gpuStats.innerText = "GPU Level: " + gpuLvl
+}
+
+function gpuPassive() {
+    bits += gpuPower
+    render()
 }
 
 bitBtn.addEventListener("click", function addBits () {
@@ -33,6 +46,22 @@ cpuBtn.addEventListener("click", function () {
         cpuCost *= 2
         cpuPower++
         cpuLvl++
+        render()
+    }
+})
+
+gpuBtn.addEventListener("click", function () {
+    if(bits < gpuCost) {
+        alert(`You need ${gpuCost} bits to buy this upgrade!`)
+    } else {
+        bits -= gpuCost
+        gpuCost *= 3
+        if(timer !== -1337) {
+            clearInterval(timer)
+        }
+        timer = setInterval(gpuPassive, 10000) // Time in ms (10sec)
+        gpuPower++
+        gpuLvl++
         render()
     }
 })

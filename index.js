2cc3f54c48b15840f6c829fb0ebb419866e3befd
Dani <daniel.varlan20@gmail.com>
2023-01-02 18:01:19 +0100

    Added saving & loading
    + Some refactoring

--- index.js
+++ index.js
@@ -4,18 +4,41 @@ const cpuBtn = document.getElementById("cpubtn")
 const cpuStats = document.getElementById("cpustats")
 const gpuBtn = document.getElementById("gpubtn")
 const gpuStats = document.getElementById("gpustats")
+const saveBtn = document.getElementById("savebtn")
+const resetBtn = document.getElementById("resetbtn")
 
 let currentClickAmount = 1
 let bits = 0
 let timer = -1337
 
-let cpuCost = 50
-let cpuPower = 1
-let cpuLvl = 0
+let cpu = {
+    cpuCost : 50,
+    cpuPower: 1,
+    cpuLvl: 0
+}
 
-let gpuCost = 100
-let gpuPower = 0
-let gpuLvl = 0
+let gpu = {
+    gpuCost : 100,
+    gpuPower : 0,
+    gpuLvl : 0
+}
+
+// Check if the player has an existing save & load it
+if (localStorage.length !== 0) {
+    const cpuInfo = JSON.parse(localStorage.getItem("cpuInfo"))
+    const gpuInfo = JSON.parse(localStorage.getItem("gpuInfo"))
+    const bitInfo = JSON.parse(localStorage.getItem("bitInfo"))
+    const clickInfo = JSON.parse(localStorage.getItem("clickInfo"))
+    
+    cpu = cpuInfo
+    gpu = gpuInfo
+    bits = bitInfo
+    currentClickAmount = clickInfo
+    
+    timer = setInterval(gpuPassive, 10000)
+
+    render()
+}
 
 function render() {
     bitText.innerText = "Bits: " + bits
@@ -23,14 +46,29 @@ function render() {
 }
 
 function renderStats() {
-    cpuStats.innerText = "CPU Level: " + cpuLvl
-    gpuStats.innerText = "GPU Level: " + gpuLvl
+    cpuStats.innerText = "CPU Level: " + cpu.cpuLvl
+    gpuStats.innerText = "GPU Level: " + gpu.gpuLvl
 }
 
 function gpuPassive() {
-    bits += gpuPower
+    bits += gpu.gpuPower
     render()
 }
+
+saveBtn.addEventListener("click", function () {
+    localStorage.setItem("cpuInfo", JSON.stringify(cpu))
+    localStorage.setItem("gpuInfo", JSON.stringify(gpu))
+    localStorage.setItem("bitInfo", JSON.stringify(bits))
+    localStorage.setItem("clickInfo", JSON.stringify(currentClickAmount))
+})
+
+resetBtn.addEventListener("click", function () {
+    // TODO: Ask user if they really want to delete the save
+    if(localStorage.getItem("cpuInfo") !== null) {
+        localStorage.clear()
+    }
+    location.reload()
+})
 
 bitBtn.addEventListener("click", function addBits () {
     bits += currentClickAmount
@@ -38,30 +76,30 @@ bitBtn.addEventListener("click", function addBits () {
 })
 
 cpuBtn.addEventListener("click", function () {
-    if(bits < cpuCost) {
-        alert(`You need ${cpuCost} bits to buy this upgrade!`)
+    if(bits < cpu.cpuCost) {
+        alert(`You need ${cpu.cpuCost} bits to buy this upgrade!`)
     } else {
-        bits -= cpuCost
-        currentClickAmount += cpuPower 
-        cpuCost *= 2
-        cpuPower++
-        cpuLvl++
+        bits -= cpu.cpuCost
+        currentClickAmount += cpu.cpuPower 
+        cpu.cpuCost *= 2
+        cpu.cpuPower++
+        cpu.cpuLvl++
         render()
     }
 })
 
 gpuBtn.addEventListener("click", function () {
-    if(bits < gpuCost) {
-        alert(`You need ${gpuCost} bits to buy this upgrade!`)
+    if(bits < gpu.gpuCost) {
+        alert(`You need ${gpu.gpuCost} bits to buy this upgrade!`)
     } else {
-        bits -= gpuCost
-        gpuCost *= 3
+        bits -= gpu.gpuCost
+        gpu.gpuCost *= 3
         if(timer !== -1337) {
             clearInterval(timer)
         }
         timer = setInterval(gpuPassive, 10000) // Time in ms (10sec)
-        gpuPower++
-        gpuLvl++
+        gpu.gpuPower++
+        gpu.gpuLvl++
         render()
     }
 })
